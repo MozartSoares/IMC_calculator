e38e09151f322de246e0c56c849ae48ef5df91f7
Mozart Soares <mozartarmstrong@gmail.com>
2023-11-23 20:15:34 -0300

    -adicionado feedback de cores para a categoria na tabela

--- src/Components/result/result.jsx
+++ src/Components/result/result.jsx
@@ -41,33 +41,33 @@ const Result = ({ imc }) => {
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td>&lt; 18.5</td>
-                                    <td>Abaixo do Peso</td>
+                                    <td className={categoria === "Abaixo do Peso" ? "bg-primary" : ""}>&lt; 18.5</td>
+                                    <td className={categoria === "Abaixo do Peso" ? "bg-primary" : ""}>Abaixo do Peso</td>
+                                </tr>
+                                <tr >
+                                    <td className={categoria === "Normal" ? "bg-primary" : ""}>18.5 - 24.9</td>
+                                    <td className={categoria === "Normal" ? "bg-primary" : ""}>Normal</td>
                                 </tr>
                                 <tr>
-                                    <td>18.5 - 24.9</td>
-                                    <td>Normal</td>
+                                    <td className={categoria === "Sobrepeso" ? "bg-primary" : ""}>25 - 29.9</td>
+                                    <td className={categoria === "Sobrepeso" ? "bg-primary" : ""}>Sobrepeso</td>
+                                </tr>
+                                <tr >
+                                    <td className={categoria === "Obesidade Grau 1" ? "bg-primary" : ""}>30 - 34.9</td>
+                                    <td className={categoria === "Obesidade Grau 1" ? "bg-primary" : ""}>Obesidade Grau 1</td>
                                 </tr>
                                 <tr>
-                                    <td>25 - 29.9</td>
-                                    <td>Sobrepeso</td>
+                                    <td className={categoria === "Obesidade Grau 2" ? "bg-primary" : ""}>35 - 39.9</td>
+                                    <td className={categoria === "Obesidade Grau 2" ? "bg-primary" : ""}>Obesidade Grau 2</td>
                                 </tr>
                                 <tr>
-                                    <td>30 - 34.9</td>
-                                    <td>Obesidade Grau 1</td>
-                                </tr>
-                                <tr>
-                                    <td>35 - 39.9</td>
-                                    <td>Obesidade Grau 2</td>
-                                </tr>
-                                <tr>
-                                    <td>&gt;= 40</td>
-                                    <td>Obesidade Grau 3</td>
+                                    <td className={categoria === "Obesidade Grau 3" ? "bg-primary" : ""}>&gt;= 40</td>
+                                    <td className={categoria === "Obesidade Grau 3" ? "bg-primary" : ""}>Obesidade Grau 3</td>
                                 </tr>
                             </tbody>
                         </table>
                     </>
-                ):<p className="text-light h5">Preencha os campos e clique em ver resultado para exibir sua classificação na tabela</p>
+                ) : <p className="text-light h5">Preencha os campos e clique em ver resultado para exibir sua classificação na tabela</p>
                 }
             </div>
         </>
